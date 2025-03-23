9cd47f954d9c18585ecc4afcc185d316dd1eace6
zongjiewang92 <zongjiewang92@gmail.com>
2025-03-23 14:19:14 -0700

    log

--- scraper_02.py
+++ scraper_02.py
@@ -209,6 +209,7 @@ def scrape_questions(step3, question_set, max_questions=25):
             if now_question in question_set:
                 print(f"✅ 题目已经存在，不抓取答案， 跳过题目：{now_question}")
             else:
+                print(f"✅ 题目不存在，取答案")
                 take_screenshot(driver, "question", 2)
                 # **获取正确答案**
                 try:
